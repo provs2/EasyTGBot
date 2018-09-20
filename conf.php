07cf523c9116d0f93a5eefd7f8c9f1546b48d45c
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2018-09-20 19:23:01 +0200

    Added table_name

--- conf.php
+++ conf.php
@@ -5,10 +5,9 @@ $TGBot->settings(['disable_web_page_preview' => false,
 'MySQL' => false,
 'adminMySQL' => false, //Use MySQL for global post etc
 'adminPostGreSQL' => false, //Use PostGreSQL for global post etc
+'table_name' => 'EasyTGBot',
 'admins' => [
-    451540728,
-    432234432,
-    242343244
+    685380727
 ]
 ]);
 $TGBot->PostgreDBCredentials('localhost', 'USERNAME', 'PASSWORD', 'DATABASE');
